fb5c6391fa17a2e1e70a34ebe02c98dac892b5d5
lusk70 <78473269+lusk70@users.noreply.github.com>
2022-06-30 10:02:04 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [account, setAccount] = useState("-");
+  const [chainId, setChainId] = useState("-");
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const ConnectWalletHandler = async () => {
+    if (window.ethereum) {
+      try {
+        await window.ethereum
+          .request({
+            method: "eth_requestAccounts",
+          })
+          .then((result) => {
+            setAccount(result[0]);
+          });
+        await window.ethereum
+          .request({
+            method: "eth_chainId",
+          })
+          .then((result) => {
+            setChainId(result);
+          });
+      } catch (err) {
+        setErrorMessage(err);
+      }
+    } else {
+      setErrorMessage("Need to install Metamask!!");
+    }
+  };
+
+  useEffect(() => {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", ConnectWalletHandler);
+      window.ethereum.on("chainChanged", ConnectWalletHandler);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>MetaMask Test</p>
+      <button onClick={ConnectWalletHandler}>Connect Wallet</button>
+      <p>Connected with: {account}</p>
+      <p>chainId is: {chainId}</p>
+      {errorMessage ? <p>Error: {errorMessage}</p> : null}
     </div>
   );
 }
